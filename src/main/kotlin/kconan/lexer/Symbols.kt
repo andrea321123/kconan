72c6d2ec8b956c2ab4269289d17031446b26fe21
andrea321123 <gal.andrea.van@gmail.com>
2020-12-28 18:01:29 +0100

    Add maps (Symbol/Keyword) -> TokenType

--- src/main/kotlin/kconan/lexer/Symbols.kt
+++ src/main/kotlin/kconan/lexer/Symbols.kt
@@ -1,7 +1,9 @@
 // Symbols.kt
-// Version 1.0.0
+// Version 1.0.1
 
 package kconan.lexer
+
+import kconan.token.TokenType
 
 // set of all valid first symbols of a word
 val validFirstLetters = setOf(
@@ -45,3 +47,71 @@ val conanWords = setOf(
     "i8","u8","i16","u16","i32","u32","i64","u64","f32","f64",
     "true","false","null",
     "and","or","not")
+
+// map each symbol to a tokenType
+val symbolsToToken = mapOf(
+    "&" to TokenType.BITWISE_AND,
+    "|" to TokenType.BITWISE_OR,
+    "~" to TokenType.BITWISE_XOR,
+    "!" to TokenType.BITWISE_NOT,
+    "," to TokenType.COMMA,
+    "." to TokenType.PERIOD,
+    ";" to TokenType.SEMICOLON,
+    ":" to TokenType.COLON,
+    "{" to TokenType.OPENING_CURLY_BRACKET,
+    "}" to TokenType.CLOSING_CURLY_BRACKET,
+    "(" to TokenType.OPENING_PARENTHESIS,
+    ")" to TokenType.CLOSING_PARENTHESIS,
+    "[" to TokenType.OPENING_SQUARE_BRACKET,
+    "]" to TokenType.CLOSING_SQUARE_BRACKET,
+    "=" to TokenType.ASSIGN,
+    "+" to TokenType.ADDITION,
+    "-" to TokenType.SUBTRACTION,
+    "*" to TokenType.MULTIPLICATION,
+    "/" to TokenType.DIVISION,
+    "%" to TokenType.REMAINDER,
+    "==" to TokenType.EQUALS_TO,
+    "!=" to TokenType.NOT_EQUALS_TO,
+    ">" to TokenType.GREATER_THAN,
+    ">=" to TokenType.GREATER_OR_EQUALS,
+    "<" to TokenType.LESS_THAN,
+    "<=" to TokenType.LESS_OR_EQUALS,
+    "++" to TokenType.INCREMENT,
+    "--" to TokenType.DECREMENT,
+    "+=" to TokenType.ASSIGNMENT_BY_SUM,
+    "-=" to TokenType.ASSIGNMENT_BY_DIFFERENCE,
+    "*=" to TokenType.ASSIGNMENT_BY_PRODUCT,
+    "/=" to TokenType.ASSIGNMENT_BY_QUOTIENT,
+    "%=" to TokenType.ASSIGNMENT_BY_REMAINDER)
+
+// map each Conan word to a tokenType
+val keywordToToken = mapOf(
+    "if" to TokenType.IF_KEYWORD,
+    "else" to TokenType.ELSE_KEYWORD,
+    "while" to TokenType.WHILE_KEYWORD,
+    "for" to TokenType.FOR_KEYWORD,
+    "return" to TokenType.RETURN_KEYWORD,
+    "break" to TokenType.BREAK_KEYWORD,
+    "continue" to TokenType.CONTINUE_KEYWORD,
+    "import" to TokenType.IMPORT_KEYWORD,
+    "as" to TokenType.AS_KEYWORD,
+    "this" to TokenType.THIS_KEYWORD,
+    "var" to TokenType.VAR_KEYWORD,
+    "fun" to TokenType.FUN_KEYWORD,
+    "struct" to TokenType.STRUCT_KEYWORD,
+    "i8" to TokenType.I8_TYPE,
+    "u8" to TokenType.U8_TYPE,
+    "i16" to TokenType.I16_TYPE,
+    "u16" to TokenType.U16_TYPE,
+    "i32" to TokenType.I32_TYPE,
+    "u32" to TokenType.U32_TYPE,
+    "i64" to TokenType.I64_TYPE,
+    "u64" to TokenType.U64_TYPE,
+    "f32" to TokenType.F32_TYPE,
+    "f64" to TokenType.F64_TYPE,
+    "true" to TokenType.TRUE_CONSTANT,
+    "false" to TokenType.FALSE_CONSTANT,
+    "null" to TokenType.NULL_CONSTANT,
+    "and" to TokenType.LOGICAL_AND,
+    "or" to TokenType.LOGICAL_OR,
+    "not" to TokenType.LOGICAL_NOT)
