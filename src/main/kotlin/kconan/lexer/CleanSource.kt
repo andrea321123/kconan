69fdb1a6bd6819d85ee44d32324756a2fe0b5abe
andrea321123 <gal.andrea.van@gmail.com>
2020-12-28 13:09:34 +0100

    Add uncomment() and skipString() function

---
+++ src/main/kotlin/kconan/lexer/CleanSource.kt
@@ -0,0 +1,62 @@
+// CleanSource.kt
+// Version 1.0.0
+
+package kconan.lexer
+
+import kconan.error.Error
+import kconan.error.ErrorType
+
+// Remove comments from source code
+// comments are single-line and start with "//"
+fun uncomment(sourceCode: String): String {
+    var i = 0
+    var newSourceCode = "" + sourceCode
+
+    while (i < newSourceCode.length -1) {
+        // we skip strings because strings contains "//"
+        if (newSourceCode[i] == '\"') {
+            i = skipString(newSourceCode, i)
+        }
+        if (newSourceCode[i] == '/' && newSourceCode[i +1] == '/') {
+            // remove all text between "//" and "\n" (inclusive)
+            val startIndex = i
+            var endIndex = i-- +2
+
+            // reach the newline character
+            while (endIndex < newSourceCode.length &&
+                newSourceCode[endIndex] != '\n') {
+                endIndex++
+            }
+
+            // if comment is at the end of file, we don't need to add 1 at endIndex
+            if (endIndex != newSourceCode.length) {
+                endIndex++
+            }
+
+            newSourceCode = newSourceCode.removeRange(startIndex, endIndex)
+        }
+        i++
+    }
+
+    return newSourceCode
+}
+
+// return the index of the first character after the string at [startIndex]
+// sourceCode[startIndex] must be '"' (double quote)
+fun skipString(sourceCode: String, startIndex: Int): Int {
+    var endIndex = startIndex + 1
+    while (endIndex < sourceCode.length) {
+        if (sourceCode[endIndex] == '\\') {
+            endIndex++
+        }
+
+        if (sourceCode[endIndex] == '\"') {
+            return  endIndex +1
+        }
+
+        endIndex++
+    }
+
+    // if arrive here, no closing " is found
+    throw Error(ErrorType.COMPILE_ERROR, "no closing double quote")
+}
---
+++ src/test/kotlin/kconan/lexer/CleanSourceTest.kt
@@ -0,0 +1,54 @@
+// CleanSourceTest.kt
+// Version 1.0.0
+
+package kconan.lexer
+
+import kconan.error.Error
+import kconan.io.readFile
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CleanSourceTest {
+    private val conanSourcesDirectory = "src/test/resources/conan-files/"
+    private fun countSubstring(string: String, substring: String): Int =
+        string.split(substring).size -1
+
+    @Test
+    fun skipStringTest() {
+        var test = "This string contains \"another string\" inside"
+        assertEquals(37, skipString(test, 21))
+        test = "\" string not closed"
+        try {
+            skipString(test, 0)
+            assert(false)
+        } catch (e: Error) {
+            assertEquals(1, countSubstring(e.toString(), "no closing double quote"))
+        }
+    }
+
+    @Test
+    fun factorialUncommentTest() {
+        val test = uncomment(readFile("${conanSourcesDirectory}factorial.cn"))
+        assertEquals(126, test.length)
+        assertEquals(1, countSubstring(test, "factorial"))
+        assertEquals(9, countSubstring(test, "\n"))
+        assertEquals(0, countSubstring(test, "//"))
+    }
+
+    @Test
+    fun helloWorldUncommentTest() {
+        val test = uncomment(readFile("${conanSourcesDirectory}helloWorld.cn"))
+        assertEquals(60, test.length)
+        assertEquals(1, countSubstring(test, "Hello, world!"))
+        assertEquals(3, countSubstring(test, "\n"))
+    }
+
+    @Test
+    fun commentUncommentTest() {
+        val test = uncomment(readFile("${conanSourcesDirectory}comment.cn"))
+        assertEquals(51, test.length)
+        assertEquals(2, countSubstring(test, "//"))
+        assertEquals(3, countSubstring(test, "\n"))
+    }
+}
