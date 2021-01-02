9d42bc75e5e35a72f21742fafdd72eb9203f36ce
andrea321123 <gal.andrea.van@gmail.com>
2021-01-02 19:02:52 +0100

    Create TokenToTreeToken.kt

---
+++ src/main/kotlin/kconan/parser/TokenToTreeToken.kt
@@ -0,0 +1,18 @@
+// TokenToTreeToken.kt
+// Version 1.0.0
+
+package kconan.parser
+
+import kconan.token.TokenType
+
+val tokenToTreeToken = mapOf(
+    TokenType.U8_TYPE to TreeTokenType.U8_TYPE,
+    TokenType.I8_TYPE to TreeTokenType.I8_TYPE,
+    TokenType.U16_TYPE to TreeTokenType.U16_TYPE,
+    TokenType.I16_TYPE to TreeTokenType.I16_TYPE,
+    TokenType.U32_TYPE to TreeTokenType.U32_TYPE,
+    TokenType.I32_TYPE to TreeTokenType.I32_TYPE,
+    TokenType.U64_TYPE to TreeTokenType.U64_TYPE,
+    TokenType.I64_TYPE to TreeTokenType.I64_TYPE,
+    TokenType.F32_TYPE to TreeTokenType.F32_TYPE,
+    TokenType.F64_TYPE to TreeTokenType.F64_TYPE)
