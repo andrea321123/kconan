88ac08c784a6fc37c38475c7da47d296d0a4fd20
andrea321123 <gal.andrea.van@gmail.com>
2021-01-02 19:03:08 +0100

    Create TokenTypeGroups.kt

---
+++ src/main/kotlin/kconan/token/TokenTypeGroups.kt
@@ -0,0 +1,17 @@
+// TokenTypeGroups.kt
+// Version 1.0.0
+
+package kconan.token
+
+// set of Conan types
+val types = setOf(
+    TokenType.I8_TYPE,
+    TokenType.U8_TYPE,
+    TokenType.I16_TYPE,
+    TokenType.U16_TYPE,
+    TokenType.I32_TYPE,
+    TokenType.U32_TYPE,
+    TokenType.I64_TYPE,
+    TokenType.U64_TYPE,
+    TokenType.F32_TYPE,
+    TokenType.F64_TYPE)
