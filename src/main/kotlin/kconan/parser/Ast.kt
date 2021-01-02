61032e050330708bb18ccf8aa48e7c5a37e78362
andrea321123 <gal.andrea.van@gmail.com>
2021-01-02 13:46:41 +0100

    Create AST class

---
+++ src/main/kotlin/kconan/parser/Ast.kt
@@ -0,0 +1,49 @@
+// Ast.kt
+// Version 1.0.0
+
+package kconan.parser
+
+// Implement an Abstract syntax tree
+class Ast(val head: TreeToken) {
+    val children: ArrayList<Ast> = ArrayList()
+
+    fun add(child: Ast) {
+        children.add(child)
+    }
+
+    override fun toString(): String {
+        return toStringRec(0)
+    }
+
+    // Return the number of treeToken are in the AST
+    fun size(): Long {
+        // exit condition
+        if (children.size == 0) {
+            return 1
+        }
+
+        var size: Long = 1
+        for (i in children) {
+            size += i.size()
+        }
+
+        return size
+    }
+
+    private fun toStringRec(indent: Int): String {
+        // we add the indentation
+        var returnString = ""
+        for (i in 0 until indent) {
+            returnString += " "
+        }
+
+        returnString += head.toString() + "\n"
+
+        // we call toStringRec for each child with new indent
+        for (i in children) {
+            returnString += i.toStringRec(indent +2)
+        }
+
+        return returnString
+    }
+}
---
+++ src/test/kotlin/kconan/parser/AstTest.kt
@@ -0,0 +1,46 @@
+// AstTest.kt
+// Version 1.0.0
+
+package kconan.parser
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AstTest {
+    @Test
+    fun sizeTest() {
+        // create by hand the AST of the program "var foo : u4 = 3 + 9
+        val n11 = Ast(TreeToken(TreeTokenType.IDENTIFIER, "foo", 1, 5))
+        val n121 = Ast(TreeToken(TreeTokenType.U64_TYPE, "",1, 11))
+        val n13111 = Ast(TreeToken(TreeTokenType.INTEGER_CONSTANT, "3", 1, 17))
+        val n1321 = Ast(TreeToken(TreeTokenType.ADDITION, "", 1, 19))
+        val n13311 = Ast(TreeToken(TreeTokenType.INTEGER_CONSTANT, "9", 1, 21))
+
+        val n12 = Ast(TreeToken(TreeTokenType.NUMERIC_TYPE, "", 1, 11))
+        n12.add(n121)
+        val n1311 = Ast(TreeToken(TreeTokenType.NUMBER, "", 1, 17))
+        n1311.add(n13111)
+        val n131 = Ast(TreeToken(TreeTokenType.EXP, "", 1, 17))
+        n131.add(n1311)
+        val n132 = Ast(TreeToken(TreeTokenType.OPERATOR, "", 1, 19))
+        n132.add(n1321)
+        val n1331 = Ast(TreeToken(TreeTokenType.NUMBER, "", 1, 21))
+        n1331.add(n13311)
+        val n133 = Ast(TreeToken(TreeTokenType.EXP, "", 1, 21))
+        n133.add(n1331)
+        val n13 = Ast(TreeToken(TreeTokenType.EXP, "", 1, 17))
+        n13.add(n131)
+        n13.add(n132)
+        n13.add(n133)
+
+        val n1 = Ast(TreeToken(TreeTokenType.VAR_INIT, "", 1, 1))
+        n1.add(n11)
+        n1.add(n12)
+        n1.add(n13)
+
+        val head = Ast(TreeToken(TreeTokenType.PROGRAM, "", 1, 1))
+        head.add(n1)
+
+        assertEquals(14, head.size())
+    }
+}
