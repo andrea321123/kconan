a286505609df712d55e2165851f22d912dbf013b
andrea321123 <gal.andrea.van@gmail.com>
2021-01-03 11:19:17 +0100

    Update TokenToTreeToken.kt

--- src/main/kotlin/kconan/parser/TokenToTreeToken.kt
+++ src/main/kotlin/kconan/parser/TokenToTreeToken.kt
@@ -1,5 +1,5 @@
 // TokenToTreeToken.kt
-// Version 1.0.0
+// Version 1.0.1
 
 package kconan.parser
 
@@ -15,4 +15,8 @@ val tokenToTreeToken = mapOf(
     TokenType.U64_TYPE to TreeTokenType.U64_TYPE,
     TokenType.I64_TYPE to TreeTokenType.I64_TYPE,
     TokenType.F32_TYPE to TreeTokenType.F32_TYPE,
-    TokenType.F64_TYPE to TreeTokenType.F64_TYPE)
+    TokenType.F64_TYPE to TreeTokenType.F64_TYPE,
+
+    TokenType.IDENTIFIER to TreeTokenType.IDENTIFIER,
+    TokenType.INTEGER_CONSTANT to TreeTokenType.INTEGER_CONSTANT,
+    TokenType.FLOAT_CONSTANT to TreeTokenType.FLOAT_CONSTANT)
