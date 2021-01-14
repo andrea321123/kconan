a92257a0f8736aff30dacaada79cda4b29a6e99a
andrea321123 <gal.andrea.van@gmail.com>
2021-01-14 21:26:45 +0100

    Program rule now can contain function declarations

--- src/main/kotlin/kconan/parser/grammar/ParseProgram.kt
+++ src/main/kotlin/kconan/parser/grammar/ParseProgram.kt
@@ -1,5 +1,5 @@
 // ParseProgram.kt
-// Version 1.0.2
+// Version 1.0.3
 
 package kconan.parser.grammar
 
@@ -14,17 +14,18 @@ import kconan.error.ErrorType
 
 import java.lang.IndexOutOfBoundsException
 
-// program = var_init;
-// program = var_init program;
+// program = var_init | fun_declaration;
+// program = (var_init | fun_declaration) program;
 fun parseProgram(i: Int, list: ArrayList<Token>): ParsingResult {
     var i = i
+    var valid = false
     val head = Ast(
         TreeToken(
             TreeTokenType.PROGRAM,
             "", list[i].line, list[i].column)
     )
 
-    // first child must be var init
+    // first child can be var init
     var result: ParsingResult
     try {
         result = parseVarInit(i, list)
@@ -33,20 +34,40 @@ fun parseProgram(i: Int, list: ArrayList<Token>): ParsingResult {
         throw Error(ErrorType.COMPILE_ERROR, "Expected var init",
         list[i].line, list[i].column)
     }
-
-    if (!result.result) {
-        throw Error(
-            ErrorType.COMPILE_ERROR, "Expected var init",
-            list[i].line, list[i].column)
+    if (result.result) {
+        valid = true
+        i = result.index
     }
-    i = result.index
+
+    if (!valid) {
+        // first child can be a fun_declaration
+        try {
+            result = parseFunction(i, list)
+        }
+        catch (e: IndexOutOfBoundsException) {
+            throw Error(ErrorType.COMPILE_ERROR, "Expected function declaration",
+                list[i].line, list[i].column)
+        }
+        if (result.result) {
+            valid = true
+            i = result.index
+        }
+    }
+
+    // if we didn't found a var_init or a fun_declaration,
+    // throw an error
+    if (!valid) {
+        throw Error(ErrorType.COMPILE_ERROR, "Invalid input")
+    }
+
     head.add(result.tree)
 
-    // check if there are other var init
+    // check if we are at the end of file
     if (i == list.size) {
         return ParsingResult(true, head, i)
     }
+
     result = parseProgram(i, list)
     head.add(result.tree)
-    return ParsingResult(true, head, i)
+    return ParsingResult(true, head, result.index)
 }
--- src/test/kotlin/kconan/parser/grammar/ParseProgramTest.kt
+++ src/test/kotlin/kconan/parser/grammar/ParseProgramTest.kt
@@ -1,11 +1,12 @@
 // ParseProgramTest.kt
-// Version 1.0.0
+// Version 1.0.1
 
 package kconan.parser.grammar
 
 import kconan.error.Error
 import kconan.io.readFile
 import kconan.lexer.doLexing
+import kconan.parser.token.TreeTokenType
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -21,7 +22,7 @@ class ParseProgramTest {
             test = parseProgram(0, doLexing("fun foo() {}"))
             assert(false)
         } catch(e: Error) {
-            assert(e.toString().contains("Expected var init"))
+            assert(e.toString().contains("Expected ':'"))
         }
     }
 
@@ -31,5 +32,12 @@ class ParseProgramTest {
             doLexing(readFile("${conanSourcesDirectory}varInit.cn")))
 
         assertEquals(18, test.tree.size())
+    }
+
+    @Test
+    fun parseProgramTest3() {
+        val source = readFile("${conanSourcesDirectory}a.cn")
+        val test = parseProgram(0, doLexing(source))
+        assertEquals(TreeTokenType.FUNCTION, test.tree.children[1].children[0].head.token)
     }
 }
