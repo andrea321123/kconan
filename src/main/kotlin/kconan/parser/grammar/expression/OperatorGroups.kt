f4c74b0f636bd53a887cd5443baf7aed2cb8baea
andrea321123 <gal.andrea.van@gmail.com>
2021-01-17 09:42:27 +0100

    Create operators sets based on precedence rules

---
+++ src/main/kotlin/kconan/parser/grammar/expression/OperatorGroups.kt
@@ -0,0 +1,23 @@
+// OperatorGroups.kt
+// Version 1.0.0
+
+package kconan.parser.grammar.expression
+
+import kconan.parser.token.TreeTokenType
+
+val pOperators = setOf(
+    TreeTokenType.MULTIPLICATION,
+    TreeTokenType.DIVISION)
+
+val sOperators = setOf(
+    TreeTokenType.ADDITION,
+    TreeTokenType.SUBTRACTION)
+
+val cOperators = setOf(
+    TreeTokenType.EQUALS_TO,
+    TreeTokenType.NOT_EQUALS_TO,
+    TreeTokenType.GREATER_THAN,
+    TreeTokenType.GREATER_OR_EQUALS,
+    TreeTokenType.LESS_THAN,
+    TreeTokenType.LESS_OR_EQUALS)
+
