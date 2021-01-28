0e4418e2684eaeee785b828098f0786f00a9d3c2
andrea321123 <gal.andrea.van@gmail.com>
2021-01-28 12:23:38 +0100

    Add IdContainer data structure

---
+++ src/main/kotlin/kconan/semantic/name/IdContainer.kt
@@ -0,0 +1,46 @@
+// IdContainer
+// Version 1.0.0
+
+package kconan.semantic.name
+
+import java.util.HashSet
+import kotlin.collections.ArrayList
+
+// Data structure used to resolve all variable and function names
+// Internally it uses an ArrayList<HashSet> as a stack where the last set is the inner scope
+// The outer scope is the global scope
+// A new scope can be created by function, if, and while
+// Note that conan allows shadowing
+class IdContainer {
+    private val stack = ArrayList<HashSet<String>>()
+
+    init {
+        push()
+    }
+
+    // create a new scope on top of the stack
+    public fun push() {
+        stack.add(HashSet())
+    }
+
+    // delete the inner scope from the stack
+    public fun pop() {
+        stack.removeAt(stack.size -1)
+    }
+
+    // add an identifier to the last scope
+    public fun add(id: String) {
+        stack.last().add(id)
+    }
+
+    // check if an identifier is present on the stack
+    public fun contains(id: String): Boolean {
+        // search for the id from the last scope to the global scope
+        for (i in stack.size -1 downTo 0) {
+            if (stack[i].contains(id)) {
+                return true
+            }
+        }
+        return false
+    }
+}
---
+++ src/test/kotlin/kconan/semantic/name/IdContainerTest.kt
@@ -0,0 +1,24 @@
+// IdContainerTest.kt
+// Version 1.0.0
+
+package kconan.semantic.name
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+
+class IdContainerTest {
+    @Test
+    fun idContainerTest() {
+        val test = IdContainer()
+        test.add("foo")
+        test.add("bar")
+        test.push()
+        test.add("foo")     // shadowing
+        test.add("c")
+        assert(test.contains("c"))
+        assert(test.contains("foo"))
+        test.pop()
+        assertFalse(test.contains("c"))
+        assert(test.contains("foo"))
+    }
+}
