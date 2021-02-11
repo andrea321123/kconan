f8631c322b864ae6484dc509e1b8dae599115995
andrea321123 <gal.andrea.van@gmail.com>
2021-02-11 10:05:19 +0100

    Interpreter is now a class

--- src/main/kotlin/kconan/interpreter/Interpreter.kt
+++ src/main/kotlin/kconan/interpreter/Interpreter.kt
@@ -1,5 +1,5 @@
 // Interpreter.kt
-// Version 1.0.0
+// Version 1.0.1
 
 package kconan.interpreter
 
@@ -16,70 +16,73 @@ import kconan.util.ScopeMap
 // - if, else and while must have {} brackets
 // - doesn't check if each function always reach a return statement
 
-fun run(ast: Ast) {
+// Class that allows to interpret an ast
+class Interpreter(val ast: Ast) {
+    private val scope = getGlobalVariables()
+    private val functionList = getFunctionAst(ast)
 
-    val functionList = getFunctionAst(ast)
+    fun run(ast: Ast) {
+    }
 
-}
+    // Add to a ScopeMap<Integer> all global variables
+    private fun getGlobalVariables(): ScopeMap<Int> {
+        val map = ScopeMap<Int>()
+        var currentNode = ast
 
-// Add to a ScopeMap<Integer> all global variables
-fun getGlobalVariables(ast: Ast): ScopeMap<Int> {
-    val map = ScopeMap<Int>()
-    var currentNode = ast
+        while (currentNode.children.size == 2) {
+            // one child is a function or a global variable,
+            // the other is another program
+            if (currentNode.children[0].head.token == TreeTokenType.VAR_INIT) {
+                map.add(currentNode.children[0].children[0].head.value,
+                    solveExp(currentNode.children[0].children[2].children[0]))
+            }
+            currentNode = currentNode.children[1]
+        }
 
-    while (currentNode.children.size == 2) {
-        // one child is a function or a global variable,
-        // the other is another program
+        // we resolve the last part of the program
         if (currentNode.children[0].head.token == TreeTokenType.VAR_INIT) {
             map.add(currentNode.children[0].children[0].head.value,
-                solveExp(currentNode.children[0].children[2].children[0], map))
+                solveExp(currentNode.children[0].children[0].children[2]))
         }
-        currentNode = currentNode.children[1]
+
+        return map
     }
 
-    // we resolve the last part of the program
-    if (currentNode.children[0].head.token == TreeTokenType.VAR_INIT) {
-        map.add(currentNode.children[0].children[0].head.value,
-            solveExp(currentNode.children[0].children[0].children[2], map))
+    // Evaluates an expression
+    fun solveExp(ast: Ast): Int {
+        if (ast.head.token == TreeTokenType.INTEGER_CONSTANT) {
+            return Integer.parseInt(ast.head.value)
+        }
+        if (ast.children.size == 1) {
+            return solveExp(ast.children[0])
+        }
+        if (ast.head.token == TreeTokenType.IDENTIFIER) {
+            return scope.get(ast.head.value)!!
+        }
+
+        // if arrive here we have a situation of number operator number ... number
+        var i = 1
+        var tmp = solveExp(ast.children[0])     // store the temporary result
+        while (i < ast.children.size) {
+            tmp = operator(tmp,
+                ast.children[i],
+                solveExp(ast.children[i +1]))
+            i += 2
+        }
+
+        return tmp
     }
 
-    return map
-}
+    // apply the [operator] on [value1] and [value2]
+    private fun operator(value1: Int, operator: Ast, value2: Int): Int {
+        return when (operator.head.token) {
+            TreeTokenType.ADDITION -> value1 + value2
+            TreeTokenType.SUBTRACTION -> value1 - value2
+            TreeTokenType.MULTIPLICATION -> value1 * value2
+            TreeTokenType.DIVISION -> value1 / value2
+            TreeTokenType.REMAINDER -> value1 % value2
 
-// Evaluates an expression
-fun solveExp(ast: Ast, map: ScopeMap<Int>): Int {
-    if (ast.head.token == TreeTokenType.INTEGER_CONSTANT) {
-        return Integer.parseInt(ast.head.value)
-    }
-    if (ast.children.size == 1) {
-        return solveExp(ast.children[0], map)
-    }
-    if (ast.head.token == TreeTokenType.IDENTIFIER) {
-        return map.get(ast.head.value)!!
-    }
-
-    // if arrive here we have a situation of number operator number ... number
-    var i = 1
-    var tmp = solveExp(ast.children[0], map)     // store the temporary result
-    while (i < ast.children.size) {
-        tmp = operator(tmp,
-            ast.children[i],
-            solveExp(ast.children[i +1], map))
-        i += 2
-    }
-
-    return tmp
-}
-
-// apply the [operator] on [value1] and [value2]
-fun operator(value1: Int, operator: Ast, value2: Int): Int {
-    return when (operator.head.token) {
-        TreeTokenType.ADDITION -> value1 + value2
-        TreeTokenType.SUBTRACTION -> value1 - value2
-        TreeTokenType.MULTIPLICATION -> value1 * value2
-        TreeTokenType.DIVISION -> value1 / value2
-        TreeTokenType.REMAINDER -> value1 % value2
-
-        else -> 0
+            else -> 0
+        }
     }
 }
--- src/test/kotlin/kconan/interpreter/InterpreterTest.kt
+++ src/test/kotlin/kconan/interpreter/InterpreterTest.kt
@@ -1,5 +1,5 @@
 // Interpreter.kt
-// Version 1.0.0
+// Version 1.0.1
 
 package kconan.interpreter
 
@@ -13,17 +13,17 @@ class InterpreterTest {
     fun solveExpTest1() {
         val source = "fun main(): i32 { return 32+ 65+ 3;}"
         val tree = parse(doLexing(source)).tree
-        val map = getGlobalVariables(tree)
+        val test = Interpreter(tree)
         val exp = tree.children[0].children[3].children[0].children[0]
-        assertEquals(100, solveExp(exp, map))
+        assertEquals(100, test.solveExp(exp))
     }
 
     @Test
     fun solveExpTest2() {
         val source = "var a: i32 = 32; fun main(): i32 { return a+ 65+ 3;}"
         val tree = parse(doLexing(source)).tree
-        val map = getGlobalVariables(tree)
+        val test = Interpreter(tree)
         val exp = tree.children[1].children[0].children[3].children[0].children[0]
-        assertEquals(100, solveExp(exp, map))
+        assertEquals(100, test.solveExp(exp))
     }
 }
