38087459b34c497dc6d3a2d983d6557568fc206b
andrea321123 <gal.andrea.van@gmail.com>
2021-02-12 13:00:37 +0100

    Update Main.kt

--- src/main/kotlin/kconan/Main.kt
+++ src/main/kotlin/kconan/Main.kt
@@ -1,5 +1,17 @@
 package kconan
 
+import kconan.interpreter.Interpreter
+import kconan.io.readFile
+import kconan.lexer.doLexing
+import kconan.parser.grammar.parse
+import kconan.semantic.resolveNames
+
 fun main(args: Array<String>) {
-    println("Hello, world!")
+    val source = readFile("src/test/resources/conan-files/fibonacciI32.cn")
+    val tree = parse(doLexing(source)).tree
+    resolveNames(tree)
+    val test = Interpreter(tree)
+    val mainReturnValue = test.runFunction(test.functionMap["main"]!!, ArrayList())
+    println(mainReturnValue)
+
 }
