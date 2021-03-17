587fb107481214959e411c5de880c6e4a98b29e2
andrea321123 <gal.andrea.van@gmail.com>
2021-03-17 17:52:14 +0100

    Add call to convert() function

--- src/main/kotlin/kconan/Main.kt
+++ src/main/kotlin/kconan/Main.kt
@@ -1,5 +1,5 @@
 // Main.kt
-// Version 1.0.0
+// Version 1.0.1
 
 package kconan
 
@@ -7,12 +7,14 @@ import kconan.interpreter.Interpreter
 import kconan.io.readFile
 import kconan.lexer.doLexing
 import kconan.parser.grammar.parse
+import kconan.semantic.convert
 import kconan.semantic.resolveNames
 
 fun main(args: Array<String>) {
     val source = readFile(args[0])
     val tree = parse(doLexing(source)).tree
     resolveNames(tree)
+    convert(tree)
     val interpreter = Interpreter(tree)
     interpreter.run()
 
