f2c9be973ba1ebc90f5627075a82f05938aa7c20
andrea321123 <gal.andrea.van@gmail.com>
2021-04-02 10:08:05 +0200

    Create TypeResolution.kt

---
+++ src/main/kotlin/kconan/semantic/TypeResolution.kt
@@ -0,0 +1,13 @@
+// TypeResolution.kt
+// Version 1.0.0
+
+fun typeResolution(tables: SymbolTables) {
+
+}
+
+
+// Return the type of an expression and check
+// the correctness of all subexpressions
+fun getType(exp: Ast): TreeTokenType {
+    
+}
